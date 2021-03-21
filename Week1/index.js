1f0ae14b98a2e8f70b8de11bfa2cd4a9f5de9be5
6john <80368724+6john@users.noreply.github.com>
2021-03-20 21:10:30 -0400

    Add files via upload

---
+++ Week1/index.js
@@ -0,0 +1,23 @@
+//1
+
+let itemPrice = 10;
+let moneyAmount = 50;
+let friendAmount = 200;
+let ageYears = 20;
+let firstName = " John "
+let lastName = " Smith "
+let midInitial = " S "
+
+//2
+
+let newVariable = moneyAmount - 10;
+let newVariable2 = friendAmount / ageYears;
+let newVariable3 = firstName + midInitial + lastName;
+
+//3
+
+console.log('Console');
+console.log('Post Purchase Dollar Amount:'+ newVariable);
+console.log('Friends Made Per Year:'+ newVariable2);
+console.log('Full Name:'+ newVariable3);
+
